55614b6a0fde1d85cf737a4559e69d5ee9e2624a
Daniel Magers <daniel.magers@gmail.com>
2022-07-17 11:15:48 -0500

    commit tree 58d33061b9d53ac88f52a9f83ec4fa240aebb357

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-// Write your code here
+
+function multiply(num1,num2){ 
+    return num1 * num2;
+} 
+multiply(31,2);
+
+function random(num) {
+    return Math.floor(Math.random() * 10)
+}
+random(1)
+
+
+function mod(num3, num4) {
+    return num3 % num4
+}
+mod(64,10)
+
+function max(num5, num6, num7, num8, num9) {
+    return Math.max(num5, num6, num7, num8, num9);
+}
+max(1, 2, 3, 4, 20)
